083cfee18d198206a6d9fe0c8271f1dfc7611b78
Devansh Gupta <devansh00gupta@gmail.com>
2024-01-02 23:51:39 +0530

    check

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -43,7 +43,7 @@ export default function About() {
               href="#"
               className="px-4 py-2 text-gray-100 bg-blue-500 rounded dark:bg-blue-400 dark:hover:bg-blue-500 hover:bg-blue-600"
             >
-              Learn more
+              Learn More
             </a>
           </div>
         </div>
